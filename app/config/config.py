20f223e05fd03fd9666fd56f5ede251e03744164
David Spautz <david.spautz@freistrom.io>
2021-02-17 12:46:02 +0800

    cleanup configs

--- app/config/config.py
+++ app/config/config.py
@@ -10,13 +10,15 @@ class Config(object):
     WTF_CSRF_ENABLED = True
     CSRF_SESSION_KEY = 'notejam-flask-secret-key'
 
-    basedir = Path(__file__).parent
-    SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI', 'sqlite:///' + os.path.join(basedir, 'notejam.db'))
-
     # Logging Configurtion
     LOGGING_FORMAT = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
     LOGGING_LOCATION = 'notejam.log'
     LOGGING_LEVEL = logging.DEBUG
+
+    POSTGRES_URL = os.getenv("POSTGRES_URL", 'localhost:5432')
+    POSTGRES_USER = os.getenv("POSTGRES_USER", 'notejam')
+    POSTGRES_PW = os.getenv("POSTGRES_PW", 'password1234!')
+    POSTGRES_DB = os.getenv("POSTGRES_DB", 'notejam-db')
 
     # Mailer Configuration
     MAIL_SERVER = 'localhost'
@@ -30,23 +32,11 @@ class DevelopmentConfig(Config):
     DEVELOPMENT = True
     DEBUG = True
 
-    POSTGRES_URL = os.getenv("POSTGRES_URL", 'localhost:5432')
-    POSTGRES_USER = os.getenv("POSTGRES_USER", 'notejam')
-    POSTGRES_PW = os.getenv("POSTGRES_PW", 'password1234!')
-    POSTGRES_DB = os.getenv("POSTGRES_DB", 'notejam-db')
-
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://{user}:{pw}@{url}/{db}'.format(user=POSTGRES_USER,pw=POSTGRES_PW,url=POSTGRES_URL,db=POSTGRES_DB)
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
     
 class ProductionConfig(Config):
-    
-    POSTGRES_URL = os.getenv("POSTGRES_URL", 'localhost:5432')
-    POSTGRES_USER = os.getenv("POSTGRES_USER", 'notejam')
-    POSTGRES_PW = os.getenv("POSTGRES_PW", '')
-    POSTGRES_DB = os.getenv("POSTGRES_DB", 'notejam-db')
 
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://{user}:{pw}@{url}/{db}'.format(user=POSTGRES_USER,pw=POSTGRES_PW,url=POSTGRES_URL,db=POSTGRES_DB)
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
         
     # Mailer Configuration
     MAIL_SERVER = 'mail.freistrom.io'
@@ -60,3 +50,5 @@ class ProductionConfig(Config):
 class TestingConfig(Config):
     TESTING = True
     WTF_CSRF_ENABLED = False
+
+    SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI', 'postgresql+psycopg2://{user}:{pw}@{url}/{db}'.format(user=POSTGRES_USER,pw=POSTGRES_PW,url=POSTGRES_URL,db=POSTGRES_DB)
