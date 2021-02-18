e26bb455744d40c707155f089c0b1756470af04c
David Spautz <david.spautz@freistrom.io>
2021-02-18 15:28:35 +0800

    add secrets

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,4 @@
 from app import create_app
-#import tempfile, os
 
-#fd, db = tempfile.mkstemp()
-#os.environ['SQLALCHEMY_DATABASE_URI'] = "sqlite:///" + db
 app = create_app()
 app.run(host='0.0.0.0')
