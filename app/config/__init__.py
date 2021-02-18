b53bab6834c910645534a81872c3dbdaff8620e4
David Spautz <david.spautz@freistrom.io>
2021-02-18 10:43:22 +0800

    fix

--- app/config/__init__.py
+++ app/config/__init__.py
@@ -7,7 +7,7 @@ def init_app(app):
   print("app.config.__init__.init_app()") 
   app.config.from_object("app.config.config." + os.getenv('FLASK_ENV', 'production').capitalize() + "Config")
   
-  DotEnv().init_app(app, verbose_mode=True)
+  #DotEnv().init_app(app, verbose_mode=True)
 
   # Configure logging
   logging.basicConfig(level=app.config['LOGGING_LEVEL'],
